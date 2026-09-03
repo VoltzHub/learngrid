0e207e77b883ef57c033064d68df61c5f8320a98
VoltzHub <voltzhub56@gmail.com>
2026-09-03 17:20:26 +0100

    Changed screen height of mobile slider

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout(
 ): ReactNode {
     return (
         <main className="max-w-360 mx-auto flex h-full">
-            <section className="block xl:hidden h-screen w-screen max-w-300 max-h-400">
+            <section className="block xl:hidden h-screen w-screen max-w-300 min-h-200 max-h-400">
                 <AuthSlider />
             </section>
             <section className="hidden xl:block flex-1/2">
